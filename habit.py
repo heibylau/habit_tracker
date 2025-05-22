c7a5a63b919005b8e7df01ff8f7029c65e2ae740
Faheem-Raja <faheem.iqbal.raja@gmail.com>
2025-05-22 11:57:43 -0600

    added rectangle to the top. Started logic to create habit list

--- habit.py
+++ habit.py
@@ -1,10 +1,31 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window
 from kivy.metrics import dp
+from kivy.uix.togglebutton import ToggleButton
+from kivy.uix.label import Label
 
 class HabitWidget(Widget):
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        labels = ["Option A", "Option B", "Option C", "Option D"]
+ 
+        for text in labels:
+            row = BoxLayout(orientation='horizontal', size_hint_y=80, height=40)
+            label = Label(text=text, size_hint_x=30)
+            toggle = ToggleButton(text='Off', size_hint_x=30)
+ 
+            # Optional: Change text on toggle
+            toggle.bind(on_press=self.toggle_text)
+ 
+            row.add_widget(label)
+            row.add_widget(toggle)
+            self.add_widget(row)
+    def toggle_text(self, instance):
+        instance.text = 'on' if instance.state=='down' else 'off'
+
+
 
 class HabitApp(App):
     def build(self):
